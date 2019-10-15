78e52b5d3792274fe6eb970702b15ab172368bc9
Gordiig <gordiig@gmail.com>
2019-10-15 16:03:26 +0300

    Removed old models

--- Un_RSOI_3/VCs/UIViewControllerExt.swift
+++ Un_RSOI_3/VCs/UIViewControllerExt.swift
@@ -43,32 +43,5 @@ protocol ApiAlertPresentable where Self: AlertPresentable {
 }
 
 extension ApiAlertPresentable {
-    func apiAlert(_ err: ApiCallerError) {
-        switch err {
-        case .alamofireError:
-            alert(title: "Alamofire alert", message: "Something went wrong with alamofire")
-            
-        case .cantDecode:
-            alert(title: "Can't decode", message: "Can't decode incame data")
-            
-        case .cantEncode:
-            alert(title: "Can't encode", message: "Can't encode data for request")
-            
-        case .noHostGiven:
-            alert(title: "No host given", message: "Enter host for requests in settings")
-            
-        case .noTokenError:
-            alert(title: "No token", message: "Log out and log in again")
-            
-        case .wrongTokenError:
-            alert(title: "Wrong token", message: "Your token is wrong, maybe you are a hacker")
-            
-        case .unknownError:
-            alert(title: "Unknown error", message: "Don't know what to do here")
-            
-        case.incameError(code: let code, text: let text):
-            alert(title: "Code \(code)", message: "Incame message: \"\(text)\"")
-        }
-    }
     
 }
