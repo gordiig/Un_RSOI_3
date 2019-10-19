5ed8fe8dee3ea8d3eb8c6029b8ca0cbacef1c735
Gordiig <gordiig@gmail.com>
2019-10-19 12:44:54 +0300

    Add LogInView

---
+++ Un_RSOI_3/Views/LogInView.swift
@@ -0,0 +1,21 @@
+//
+//  LogInView.swift
+//  Un_RSOI_3
+//
+//  Created by Dmitry Gorin on 19.10.2019.
+//  Copyright © 2019 gordiig. All rights reserved.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogInView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogInView()
+    }
+}
